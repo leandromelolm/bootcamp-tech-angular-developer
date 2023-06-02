3ba0b1f0c6cdffa3b76177c9a84695937e1aa541
Leandro Melo <leandrosiqueira.pe@gmail.com>
2023-06-02 20:59:45 -0300

    Added Game Rule.  Using NgFor and ngIf

--- projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
+++ projeto-buzzfeed/src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent {
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+      
+      this.questionMaxIndex = this.questions.length
+
+    }
+
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{      
+      this.finished = true      
+    }
+  }
 
 }
